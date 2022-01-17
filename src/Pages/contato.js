d622d26ed616d47aef55fb1c29ece9f4ac70bee7
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2022-01-17 01:01:37 -0300

    Refactor: fetch from callback hell to async/await, destructuring message

--- src/Pages/contato.js
+++ src/Pages/contato.js
@@ -13,45 +13,38 @@ const Contatos = () => {
 
     useEffect( () => {
         (async()=>{
-            const response = await fetch(url)
-            const data = await response.json();
+            const data = await fetch(url).then((response) => response.json())
             setMessage(data);
         })()    
     }, [render])
 
-    const sendMessage = () => {
+    const sendMessage = async () => {
         setValidator(false);
-        if(author.length <= 0 || content.length <= 0){
-            return setValidator(!validator)
-        }
+        if(author.length <= 0 || content.length <= 0) return setValidator(!validator)
+
         const bodyForm = {
             email: author,
             message: content,
         }
 
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(bodyForm)
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            if(data.id) {
-                setRender(true);
-                setSuccess(true);
-                setTimeout(() => {
-                    setSuccess(false);
-                }, 5000)
-            }
-        })
-        
+        const {id} = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(bodyForm)
+            })
+            .then((response) => response.json())
+
+        if(id){
+            setRender(true);
+            setSuccess(true);
+            setTimeout(() => setSuccess(false), 5000)
+        }
+
         setAuthor('');
         setContent('');
-        
-        console.log(content)
-    }  
+    }
 
     return(
         <>
@@ -77,13 +70,15 @@ const Contatos = () => {
                 Sent
             </Button>
 
-            {message.map((content) => {
+            {message.map(({id, email, message, created_at}) => {
                 return(
-                    <div className="card mt-2" key={content.id}>
+                    <div className="card mt-2" key={id}>
                         <div className="card-body">
-                            <h5 className="card-title">{content.email}</h5>
-                            <p className="card-text">{content.message}</p>
-                            <p className="card-text"><small className="text-muted">{content.created_at}</small></p>
+                            <h5 className="card-title">{email}</h5>
+                            <p className="card-text">{message}</p>
+                            <p className="card-text">
+                                <small className="text-muted">{created_at}</small>
+                            </p>
                         </div>
                     </div>
                 )
